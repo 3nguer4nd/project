478ae2d88c53bddaf710964c08be819b80b0290a
Enguérand.CA <enguerand.ca@gmail.com>
2025-01-09 19:32:10 +0100

    test7

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -12,7 +12,7 @@ export default function Hero() {
         className="text-center"
       >
         <h1 className="text-5xl md:text-7xl font-bold mb-6">
-          Hi, I'am <span className="text-blue-400">Enguérand CA</span>
+          Hi, I'am <span className="text-blue-400">Enguérroooand CA</span>
         </h1>
         <p className="text-xl md:text-2xl text-gray-300 mb-8">
         Work-study student in BTS SIO, SISR option
